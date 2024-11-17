a4f08a780f37212f871dd310233d7011d91f5855
ALEX SILVA <alex.silvacb@aol.com>
2024-11-17 15:57:23 -0300

    Criação componente container

--- src/app/pages/principal-jornada/principal-jornada.component.ts
+++ src/app/pages/principal-jornada/principal-jornada.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../modules/header/header.component';
 import { BannerComponent } from '../../modules/banner/banner.component';
+import { ContainerComponent } from '../../modules/container/container.component';
 
 @Component({
   selector: 'app-principal-jornada',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, ContainerComponent],
   templateUrl: './principal-jornada.component.html',
   styleUrl: './principal-jornada.component.scss'
 })
