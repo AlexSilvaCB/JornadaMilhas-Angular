92b663bf9dbe13855e51ef2b6ad6fc0971a07b49
ALEX SILVA <alex.silvacb@aol.com>
2025-02-14 16:51:03 -0300

    Criação componente Label

--- src/app/modules/filtros-complementares/filtros-complementares.component.ts
+++ src/app/modules/filtros-complementares/filtros-complementares.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { MatIconModule } from '@angular/material/icon';
+import { CompanhiasComponent } from './companhias/companhias.component';
+import { ParadasComponent } from './paradas/paradas.component';
+import { PrecosComponent } from './precos/precos.component';
 
 @Component({
   selector: 'app-filtros-complementares',
   standalone: true,
-  imports: [CardComponent, MatIconModule],
+  imports: [CardComponent, CompanhiasComponent, ParadasComponent, PrecosComponent ,MatIconModule],
   templateUrl: './filtros-complementares.component.html',
   styleUrl: './filtros-complementares.component.scss'
 })
